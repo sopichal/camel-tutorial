bd7b4cfcaf055dfa9217df93b07944e4b0ff5ddf
stan <sopichal@behaimits.com>
2024-10-07 14:40:22 +0200

    ftp-jms with config.properties

--- chapter2/ftp-jms/src/main/java/com/behaimits/cameltutorial/FtpToJMSExample.java
+++ chapter2/ftp-jms/src/main/java/com/behaimits/cameltutorial/FtpToJMSExample.java
@@ -1,35 +1,62 @@
 package com.behaimits.cameltutorial;
 
-
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
-import org.apache.camel.impl.DefaultCamelContext;
-import jakarta.jms.ConnectionFactory;
-import jakarta.jms.JMSException;
+// import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
+import org.apache.camel.component.properties.PropertiesComponent;
+// import jakarta.jms.ConnectionFactory;
 
 public class FtpToJMSExample {
   public static void main(String args[]) throws Exception {
-    CamelContext context = new DefaultCamelContext(); 
 
-  org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory amqcf = new  org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory("vm://localhost");
-  jakarta.jms.ConnectionFactory cf = amqcf;
+    Main main = new Main();
 
-  // Create the Camel JMS component and wire it to our broker connection factory
-  JmsComponent jms = new JmsComponent();
-  jms.jmsComponentAutoAcknowledge(cf);
+    // // ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616?user=artemis&password=artemis");
+    // ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(tcp://localhost:61616)?initialReconnectDelay=1000");
+    // connectionFactory.setUser("artemis");
+    // connectionFactory.setPassword("artemis");
 
-    context.addComponent("jms", jms);
-    context.addRoutes(new RouteBuilder() { 
-      public void configure() {
-        from("ftp://rider.com/orders?username=rider&password=secret").to("jms:incomingOrders");
-        //from("ftp://localhost:2121/orders?username=rider&password=secret")
+    main.configure().addRoutesBuilder(new RouteBuilder() {
+      @Override
+      public void configure() throws Exception {
+        // Get the Camel context
+        CamelContext context = this.getCamelContext();
+
+        PropertiesComponent propertiesComponent = new PropertiesComponent();
+        propertiesComponent.setLocation("classpath:config.properties");
+
+        context.setPropertiesComponent(propertiesComponent);
+
+        // Resolve the properties manually before using them
+        String brokerUrl = context.resolvePropertyPlaceholders("{{broker.url}}");
+        String brokerUser = context.resolvePropertyPlaceholders("{{broker.user}}");
+        String brokerPassword = context.resolvePropertyPlaceholders("{{broker.password}}");
+
+        // Create ActiveMQConnectionFactory with credentials
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+        connectionFactory.setUser(brokerUser);
+        connectionFactory.setPassword(brokerPassword);
+
+        JmsComponent jmsComponent = new JmsComponent();
+        jmsComponent.setConnectionFactory(connectionFactory);
+        jmsComponent.setPreserveMessageQos(true);
+
+        context.addComponent("jms", jmsComponent);
+
+        // from("ftp://localhost:2121/orders?username=rider&password=secret&delay=5000").to("jms:incomingOrders");
+        from("ftp://{{ftp.server}}:{{ftp.port}}/orders?username={{ftp.user}}&password={{ftp.password}}&delay=5000&move=done&autoCreate=true")
+          .to("jms:incomingOrders");
+        // from("ftp://localhost:2121/orders?username=rider&password=secret")
         //  .to("file:data/outbox");
       } 
     });
-    context.start();
-    Thread.sleep(10000);
-    context.stop();
+    // main.start();
+    // Thread.sleep(60000);
+    // main.stop();
+    main.run();
   }
 
 }
